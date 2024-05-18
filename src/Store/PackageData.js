f3cf343ccf315f84eafb38ee7204edb771832c31
chaitanyacrk03 <64172696+chaitanyacrk03@users.noreply.github.com>
2024-05-18 14:25:54 +0530

    Update PackageData.js with complete data

--- src/Store/PackageData.js
+++ src/Store/PackageData.js
@@ -115,16 +115,198 @@ const data=[
     },
     {
       id:2,
-      duration:"4 nights & 5 days package",
-      noOfPeople:"4 Adults",
-      totalPrice:"INR 88,222/-",
-    },
-    {
-      id:3,
-      duration:"2 nights & 3 days package",
-      noOfPeople:"4 Adults",
-      totalPrice:"INR 88,222/-",
-    }
+      duration:"*Duration 5 nights & 6 days *",
+      noOfPeople:"4 Adults + 1 infant",
+      totalPrice:"INR 65,551/-",
+      totalData:{
+        duration:"*Trip duration 5 nights & 6 days (Private passenger cruise proposal)*",
+        hotelTitle:"Hotels selected for the trip:",
+        hotels:[
+          "At Port Blair: Hotel Star Paradise Deluxe Sea View room on CP plan @ 3,584 x 2 rooms x 2 nights = 14,336/-",
+          "At Swaraaj Dweep (Havelock Island): Radhakrishna resort Deluxe room on CP plan @ 3,000 x 2 rooms x 2 nights = 12,000/",
+          "At Shaheed Dweep (Neil Island): Sandy Ridge resort Deluxe room on CP plan @ 2,500 x 2 rooms x 1 night = 5,000/-",
+        ],
+        totalHotelCost:"Total for the hotels on CP Plan: INR 31,336/- (Including GST)",
+        days:[
+          {
+            title:"Day 1: First day in Port Blair",
+            data:"At the Airport, the private driver will drive you to the hotel. Check into the hotel and rest for a while. Afternoon at 2pm, reach Cellular jail, this is a must visit for any person as this place has many stories to tell people who love their countries dearly. Freedom fighters of India,were prisoned here. This place for its jail was also known as Kala Paani. Death waters. One that comes here, never returns. Rightfully it is so. 1300 kms away from the main land, in between the ocean, swimming is not the option. Spend time seeing the architecture and the way prisoners were treated. After seeing this place to your contempt, head towards Corbyns Cove Beach, one of the attractive beaches in Port Blair. Indulge in some water sports there. Jet skie or a speed boat ride & parasailing to snake island will thrill you. In the evening return to the cellular jail to see the light and sound show. The light and sound show for an hour will enlighten you with the history of the Cellular Jail. After the show, head back to the hotel. End of day 1.",
+            additionalDetails:[
+              "A/c cab with private driver:",
+              "A/c cab pickup from airport and drop to hotel and trip around Port Blair: 1,175/-",
+              "Cellular jail entry tickets per head:  30 x 4 = 120/-",
+              "Cellular Jail Light and Sound show ticket per head: 300 x 4 = 1,200/-",
+              "Total: 2,495/-"
+            ]
+          },
+          {
+            title:"Day 2: Trip to Swaraaj Dweep (Havelock) Island and stay:",
+            data:"Early morning rise and the cab will pickup at 7:00am for the 8:00am cruise. Be dropped at Port Blair jetty at 7:15am. Cruise sailing to Swaraaj Dweep (Havelock) is one hour thirty minutes on a good weather day. Reach Swaraaj Dweep (Havelock) by 9:45am. A/c cab with private driver will pick you from Swaraaj Dweep (Havelock) jetty and drop you to the respective hotel. After check in, rest for a while. First trip will be a visit to kalapathar beach. It’s one of the most beautiful beaches in Swaraaj Dweep (Havelock) Island. Pristine blue water welcomes you. Spend quality time at the beach and return to the hotel or any restaurant for lunch. After lunch head towards Radhanagar beach.Radhanagar beach is the 4thbest beach in the world, rated by Trip Advisor. The sunset at Radhanagar beach is one of the best in the islands. After sunset return to the hotel and retire for the day.",
+            additionalDetails:[
+              "A/c cab with private driver:",
+              "A/c cab pickup from hotel to Port Blair jetty: 200/-",
+              "Private passenger cruise ticket to Swaraaj Dweep (Havelock) Island from Port Blair: 1,435 x 4 = 5,740/-",
+              "A/c cab pickup from Swaraaj Dweep (Havelock) Island jetty and drop to hotel: 250/-",
+              "A/c cab trip to Kalapathar & Radhanagar beach: 1,700/-",
+              "Total: 7,890/-"
+            ]
+          },
+          {
+            title:"Day 3: Elephant Beach Trip & Day for Scuba Diving (Optional)",
+            data:"Early morning rise by 7am, cab will pick you from the hotel and drop you at the dive centre. The dive centre will then take you on their private speed boat to the dive spot for diving. It will be a fun dive. After the dive is over you will return to the dive centre. Breakfast at Havelock Hotel / resort and at around 9:00am reach Havelock jetty. Our coordinator will guide you to the trekking point to Elephant beach. Elephant beach is known for its pristine beach and the coral reef with loads of fish life. Snorkelling is complimentary. You can indulge in sea walking too organized by sea link adventures.  Return to Havelock Jetty by lunch time by the speed boat. Head back to the hotel and retire for the day.",            
+            additionalDetails:[
+              "A/c cab with private driver:",
+              "A/c cab pickup from Hotel to SwaraajDweep (Havelock Jetty): 250/-",
+              "Speed boat tickets on sharing basis: 1,000 x 4 = 4,000/-",
+              "A/c cab pickup from SwaraajDweep (Havelock) jetty to Hotel: 250/-",
+              "Total: 4,500/-"
+            ]
+          },
+          {
+            title:"Day 4: Scuba Diving Day (Optional) and Visit Elephant Beach",
+            data:"Early morning rise and after breakfast & checking out of the hotel, head to Swaraaj Dweep (Havelock) jetty to board the cruise to Shaheed Dweep (Neil Island)by 8:00 am for the sailing at 10:00 am. Reach Shaheed Dweep (Neil Island) at11:10 am. A/c cab with private driver will pick you and drop you at the hotel for check in. After some rest, head to visit Bharathpur beach. One of the best beach in Shaheed Dweep (Neil Island) Indulge in some water sport activities like jet skie, glass bottom boat ride etc, on spot payments. Next visit Natural Bridge according to Low and High tide. Spend time until late evening and then head to Laxmanpur beach to see one of the best sunsets. Evening return to the hotel and retire for the day.",            
+            additionalDetails:[
+              "A/c Cab with private driver:",
+              "A/c Cab pickup from the hotel and drop to Swaraaj Dweep (Havelock) jetty: 250/-",
+              "Private passenger cruise ticket to Shaheed Dweep (Neil Island) from Swaraaj Dweep (Havelock): 1,315 x 4 = 5,260/-",
+              "A/c cab pickup and tour in Shaheed Dweep (Neil Island): 1,800/-",
+              "Total: 7,310/-"
+        ]},
+          {
+            title:"Day 5: Return from Shaheed Dweep (Neil Island) in the morning cruise and visit Subash Chandra Bose Dweep (Ross Island)",
+            data:"Early morning rise by 4:00am, the cab will pick you to drive you to sitapur beach. Sitapur beach is known for its beautiful beach and also the awesome sunrises. After the sunrise, head back to the hotel for breakfast. Checkout of the hotel and head to Shaheed Dweep (Shaheed Dweep (Neil Island)) Jetty to board the cruise back to Port Blair. Leave Shaheed Dweep (Neil Island) at 11:30 am cruise and reach Port Blair at 12:30 pm. Directly head to Aberdeen jetty to board the ferry to Subash Chandra Bose Dweep (Ross Island) at 2:00pm. Subash Chandra Bose Dweep (Ross Island)Subash Chandra Bose Dweep (Ross Island) was the first administrative island in the British rule of the islands. The island now in the ruins depicts the brilliant architecture that evolved in the time of the British rule. A walk through the island will enlighten you about the living conditions of the British in the time of the Indian freedom struggle. A visit to the museum at Subash Chandra Bose Dweep (Ross Island) will showcase the stages of development in the Island. Later in the evening return back to Aberdeen jetty and be dropped at the hotel. End of day.",
+            additionalDetails:[
+              "A/c cab with private driver:",
+              "A/c cab to view sunrise at Shaheed Dweep (Neil Island)and drop to jetty: 800/-",
+              "Private passenger cruise tickets to Port Blair from Shaheed Dweep (Neil Island): 1,355 x 4 = 5,420/-",
+              "A/c cab pickup and drop to the hotel at Port Blair: 250/-",
+              "A/c cab pickup and drop to Aberdeen jetty and back: 400/-",
+              "Subash Chandra Bose Dweep (Ross Island) speed boat ticket: 370 x 4 = 1,480/-",
+              "Total: 8,350/-",
+              "Entry ticket to Subash Chandra Bose Dweep will be borne by the guest. (40 x 4 = 160/-)"
+            ]
+          },
+          {
+            title:"Day 6: Return Home with Sweet Memory.",
+            data:"Early morning rise and after breakfast head to the airport according the flight schedule. If time provides, do some shopping on the way.",
+            additionalDetails:[
+              "A/c cab with private driver:",
+              "A/c cab pickup from the hotel and drop to airport: 400/-",
+              "Total: 400/-",
+              
+            ]
+          }
+        ],
+        totalCharges:[
+          "Total for transportation, entry tickets and speed boat tickets: INR 14,525/-",
+          "Private passenger cruise tickets: INR 16,420/-",
+          "Total: INR 30,945/-"
+        ]
+      },
+      summaryTitle:"*Package Summary:*",
+      summary:[
+        "Total for the hotels on CP Plan: INR 31,336/-",
+        "Total for transportation, entry tickets and speed boat tickets: INR 14,525/-",
+        "Private passenger cruise tickets: INR 16,420/-",
+        "Total: INR 30,945/-",
+        "**Subtotal: INR 62,281/-*",
+        "MavenAndaman Service Charge 5%: INR 3,114/-",
+        "5% GST on service charge: INR 156/-",
+        "*Grand total: INR 65,551/- on CP Plan*",
+        
+      ]
+  },
+{
+  id:3,
+  duration:"*Duration 4 nights & 5 days *",
+  noOfPeople:"2 Adults",
+  totalPrice:"INR 34,073/-",
+  totalData:{
+    duration:"*Trip duration: 4 nights & 5 Days (Private passenger cruise ticket)*",
+    hotelTitle:"Hotels selected for the trip:",
+    hotels:[
+      "At Port Blair: Hotel Star Paradise Deluxe Sea view room on CP Plan @ 3,360 x 1 room x 1 night = 3,360/-",
+      "At Swaraaj Dweep (Havelock Island): Lemon Grass beach resort Lagoon room on CP Plan @ 4,000 x 1 room x 2 nights = 8,000/-",
+      "At Shaheed Dweep (Neil Island): Tango beach resort Lagoon room on CP Plan @ 4,368 x 1 room x 1 night = 4,368/-",
+    ],
+    totalHotelCost:"Total for the hotels on CP Plan: INR 15,728/- (Including GST)",
+    days:[
+      {
+        title:"Day 1: Early arrival at Port Blair and transfer to Swaraaj Dweep (Havelock) Island ",
+        data:"Receive at the airport, A/c car with private driver will receive you and drive you to a close by restaurant and drop you at Port Blair jetty to board the cruise to Swaraaj Dweep (Havelock) in the afternoon. The sailing starts at 2:00 pm. You will reach Swaraaj Dweep (Havelock) Island at 3:30pm. Be picked up by a/c private cab and driver and drop at the hotel. After check into the hotel and some rest, head to Radhanagar beach, Asia’s best beach awarded by Trip Advisor. Radhanagar is also for its lovely sunset. After sunset return to the hotel and retire for the day.",
+        additionalDetails:[
+          "A /c cab with private driver:",
+          "A/c cab pickup from airport / drop at Port Blair sea port: 275/-",
+          "Private passenger cruise ticket to Swaraaj Dweep (Havelock) Island from Port Blair: 1,435 x 2 = 2,870/-",
+          "A/c cab pickup at Swaraaj Dweep (Havelock) Island and drop to the hotel: 250/-",
+          "A/c cab drive to Radhanagar beach and back to the hotel: 850/-",
+          "Total: 4,245/-"
+        ]
+      },
+      {
+        title:"Day 2: Scuba Diving Day (Optional) and Visit Elephant Beach",
+        data:"According to the scuba diving decision of the guests, this day we transfer the guest to Swaraajdweep jetty at to board the speed boat to reach Swaraaj Dweep (Havelock) jetty. Our coordinator will guide you to board the speed boat that will take you to Elephant beach. Elephant beach is known for its pristine beach and the coral reef with loads of fish life. Snorkelling is complimentary offered by the speed boat provider. You can indulge in sea walking too organized by sea link adventures.  Return to Swaraaj Dweep (Havelock) Jetty by lunch time. Head back to the hotel and retire for the day.",
+        additionalDetails:[
+"          A/c cab with private driver:",
+"A/c cab pickup from Hotel to Swaraajdweep jetty: 250/-",
+"Speed boat tickets on sharing basis: 1,000 x 2 = 2,000/-",
+"A/c cab pickup from Swaraaj Dweep (Havelock) jetty to Hotel: 250/-",
+"Total: 2,500/-"
+        ]
+      },
+      {
+        title:"Day 3: Trip toShaheed Dweep (Neil Island) fromSwaraaj Dweep (Havelock) Island.",
+        data:"Early morning rise and after breakfast& checking out of the hotel, head to Swaraaj Dweep (Havelock) jetty to board the cruise to Shaheed Dweep (Neil Island)by 8:00 am for the sailing at 10:00 am. Reach Shaheed Dweep (Neil Island) at 11:10 am. A/c cab with private driver will pick you and drop you at the hotel for check in. After some rest, head to visit Bharathpur beach. One of the best beach in Shaheed Dweep (Neil Island) Indulge in some water sport activities like jet skie, glass bottom boat ride etc, on spot payments. Spend time until late evening and then head to Laxmanpur beach to see one of the best sunsets. Evening return to the hotel and retire for the day.",            
+        additionalDetails:[
+          "A/c cab with private driver:",
+"A/c cab pickup from the hotel and drop to Swaraaj Dweep (Havelock) jetty: 250/-",
+"Private passenger cruise ticket to Shaheed Dweep (Neil Island) from Swaraaj Dweep (Havelock): 1,315 x 2 = 2,630/-",
+"A/c cab pickup and tour in Shaheed Dweep (Neil Island): 1,800/- ",
+"Total: 4,680/-"
+        ]
+      },
+      {
+        title:"Day 4: Return from Shaheed Dweep (Neil Island) in the morning cruise and visit Cellular Jail, Cellular Jail Light and Sound Show.",
+        data:"Early morning rise by 4:00am, the cab will pick you to drive you to Sitapur beach. Sitapur beach is known for its beautiful beach and also the awesome sunrises. After the sunrise, head back to the hotel for breakfast. Checkout of the hotel and head to Shaheed Dweep (Shaheed Dweep (Neil Island)) Jetty to board the cruise back to Port Blair. Leave Shaheed Dweep (Neil Island) at 11:30 am cruise and reach Port Blair at 12:40 pm. Be received at the jetty in Port Blair and check into the hotel. After lunch and rest at the hotel, head to visit the National Memorial the Cellular Jail. Cellular jail, this is a must visitfor any person as this place has many stories to tell people who lovetheir countries dearly. Freedom fighters of India, were prisoned here.This place for its jail was also known as Kala Paani. Death waters.Onewho comes here, never returns. Rightfully it is so. 1300 kms awayfrom the main land, in between the ocean, swimming is not the option.Spend time seeing the architecture and the way prisoners were treated. Later in the evening watch the famous Light & Sound Show at the Cellular Jail. The whole story unfolds in a hour with colourful light sequences. After the show return to the hotel and retire for the day.",            
+        additionalDetails:[
+         " A/c cab with private driver",
+"A/c cab to view sunrise at Shaheed Dweep (Neil Island) and drop to jetty: 800/-",
+"Private passenger cruise tickets to Port Blair from Shaheed Dweep (Neil Island): 1,355 x 2 = 2,710/-",
+"A/c 17 seater tempo traveller pickup and drop to the hotel at Port Blair: 250/-",
+"A/c 13 seater tempo traveller trip to Cellular Jail and return to the hotel: 400/-",
+"Cellular Jail entry tickets: 30 x 2 = 60/-",
+"Cellular Jail Light & Sound ticket: 300 x 2 = 600/-",
+"Total: 4,820/-"
+    ]},
+      {
+        title:"Day 5: Return Home with Sweet Memory.",
+        data:"Early morning rise and after breakfast head to the airport according the flight schedule. If time provides, do some shopping on the way.",
+        additionalDetails:[
+          "A/c cab with private driver:",
+"A/c cab pickup from the hotel and drop to airport: 400/-",
+"Total: 400/-",
+        ]
+      },
+    ],
+    totalCharges:[
+      "Total for transportation, entry tickets and speed boat tickets: INR 8,435/-",
+      "Private passenger cruise tickets: INR 8,210/-",
+      "Total: INR 16,645/-"
+    ]
+  },
+  summaryTitle:"*Package Summary:*",
+  summary:[
+    "Total for the hotels on CP Plan: INR 15,728/-",
+    "Total for transportation, entry tickets and speed boat tickets: INR 8,435/-",
+    "Private passenger cruise tickets: INR 8,210/-",
+    "Total: INR 16,645/-",
+    "*Sub total: INR 32,373/-*",
+    "MavenAndaman, Service Charge 5%:  INR 1,619/-",
+    "5% GST on service charge: INR 81/-",
+    "*Grand total: INR 34,073/-on CP Plan*",
+    
+  ]
+},
 
 ]
 export default data;
