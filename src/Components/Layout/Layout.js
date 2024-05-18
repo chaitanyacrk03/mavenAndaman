98ff7b28e2610a9f6a78d6637edeef2f45b8eac9
chaitanyacrk03 <64172696+chaitanyacrk03@users.noreply.github.com>
2024-05-19 01:50:30 +0530

    Update Layout.js

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import css from './Layout.module.css';
 import Navigation from '../Navigation/Navigation';
-import {Outlet} from 'react-router-dom'
+import {Outlet} from 'react-router-dom';
+import ReactWhatsapp from 'react-whatsapp';
+import { IoCallOutline } from "react-icons/io5";
 const Layout = () => {
   return (
     <div className={css.container}>
       <Navigation />
+      <ReactWhatsapp className={css.a} size={50} number="1-212-736-5000" message="Hello World!!!"><IoCallOutline color="white" size={35}/></ReactWhatsapp>
       <Outlet />
     </div>
   )
